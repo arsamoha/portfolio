88b4873c7899c2be404485f624c681a144c867b6
Arshad Mohammad <arshad.mohammad@ooma.com>
2024-07-31 02:40:30 -0700

    homepage bare bones

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 class="name1">ARSHAD MOHAMMAD</h1>
+      <h1 class="name2">أرشد محمد</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="subpages">
+        <p class="about">ABOUT ME</p>
+        <p class="resume">RESUME</p>
+        <p class="photography">PHOTOGRAPHY</p>
+        <p class="cad">CAD</p>
+        <p class="art">ART</p>
+        <p class="projects">PROJECTS</p>
+        <div class="projectnames">
+          <p class="icook">ICOOK</p>
+          <p class="gazetracker">GAZE TRACKER</p>
+          <p class="cat">CAT'S ROOM</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
