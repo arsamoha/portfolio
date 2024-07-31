7c17800ec98269f16850de98aeed511d1c207e89
arshad.mohammad <arshad.mohammad@ooma.com>
2024-07-31 15:51:09 -0700

    projects drop down menu added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // const changeArabic = document.getElementsByClassName("english");
+
+  // changeText.addEventListener("click", function() {
+  //   changeText.textContent = "أرشد محمد";
+  // });
 
   return (
-    <>
+    <div>
       <div>
-      <h1 class="name1">ARSHAD MOHAMMAD</h1>
-      <h1 class="name2">أرشد محمد</h1>
+        <h1 className="english">ARSHAD MOHAMMAD</h1>
+        <h1 className="name2">أرشد محمد</h1>
       </div>
-      <div class="subpages">
-        <p class="about">ABOUT ME</p>
-        <p class="resume">RESUME</p>
-        <p class="photography">PHOTOGRAPHY</p>
-        <p class="cad">CAD</p>
-        <p class="art">ART</p>
-        <p class="projects">PROJECTS</p>
-        <div class="projectnames">
-          <p class="icook">ICOOK</p>
-          <p class="gazetracker">GAZE TRACKER</p>
-          <p class="cat">CAT'S ROOM</p>
+      <p className="about">ABOUT ME</p>
+      <p className="resume">RESUME</p>
+      <p className="photography">PHOTOGRAPHY</p>
+      <p className="cad">CAD</p>
+      <p className="art">ART</p>
+      <div className="dropdown">
+        <button className="dropbtn">PROJECTS</button>
+        <div className="dropdown-content">
+          <a href="#">ICOOK</a>
+          <a href="#">GAZE TRACKER</a>
+          <a href="#">CAT'S ROOM</a>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
