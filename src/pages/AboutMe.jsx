0d6638ad1d82ef1a5720af9a571f57c9e476cc09
Arshad Mohammad <arsamoha@ucsc.edu>
2025-08-15 00:14:53 -0700

    about me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,8 +1,101 @@
+import "../pages/styles/AboutMe.css";
+
 export function AboutMe() {
-    return (
-      <>
-  
-      </>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>About lil ol' me</h1>
+      <p className="aboutme">
+        I&apos;m a very fidgety person. I don&apos;t know if it&apos;s that bad
+        of an issue but it&apos;s hard for me to go a long time without doing
+        something with hands. I say it&apos;s a good thing because that&apos;s
+        the reason why I&apos;ve gotten to try and stick with so many hobbies
+        over the years. That and I steal every hobby my older sister shows an
+        interest in and make it my entire personality.{" "}
+      </p>
+      <h2>Photography</h2>
+      <p className="aboutme">
+        I first started doing photography in seventh grade by taking pictures on
+        my 5th generation iPod touch and editing them using whatever app was
+        free on the app store at the time. I made a lot of pictures that were
+        heavily inspired by tumblr aesthetics and default iPhone wallpapers. In
+        the moment I thought they were the most beautiful pictures that my{" "}
+        <a href="https://www.instagram.com/arshaddmohammad/">instagram</a>{" "}
+        followers had the honor of viewing but in exposing myself more and more
+        to actual, professional photographers&apos; works I realized how wrong I
+        was. This didn&apos;t slow me down though, I had so much fun in this
+        process I just kept taking pictures and editing them trying to make
+        every new picture better than the last. I still do that today, and
+        though I am much harder on myself about the art I create I am still in
+        love with the act of photographing and don&apos;t plan on stopping any
+        time soon.
+      </p>
+      <h2>Birding</h2>
+      <p className="aboutme">
+        This one is a new one that has consumed me mind, body, and soul. I was
+        helping my sister with her residency interview prep when she mentioned
+        her hobby was birding. This came as a shock to me because I had always
+        considered it a hobby that would only appeal to retired, wise elders but
+        seeing my sister so enthralled with it naturally made me feel fomo and I
+        just had to try it out for myself.
+      </p>
+      <p className="aboutme">
+        Fast forward to my very first birding sesh (on my birthday!) in Santa
+        Cruz, where I brought a little notepad my roommate bought for me in
+        Hawaii with a list of some birds I was hoping to spot. We hiked around
+        for about some 20 minutes without spotting a single bird. I could hear
+        them but not see them, it felt like every single bird there had grouped
+        up with a personal vendetta against me. That was until we noticed a
+        large clearing coming up on the trail. The birds got louder and louder
+        and the trail got steeper and steeper as we kept walking up towards it.
+        The trail ended at a cliff and below this cliff was one of the most
+        beautiful lakes I had ever seen with dozens of Brandt cormorants flying
+        all over the water and resting in the trees. If you&apos;ve seen How To
+        Train Your Dragon 2, I was Hiccup discovering all the dragons flying
+        around in the Land of the Dragons.
+      </p>
+      <p className="aboutme">
+        Thinking that was the highlight of my first birding experience and
+        nothing could top that, we turned around to walk back to the parking
+        lot. As soon as we walked maybe five feet back up the trail a large
+        Red-shouldered Hawk flew up and sat right in the branch of a tree in
+        front of us. I had never seen a hawk up so close and for so long before.
+        It sat there for about five minutes before swooping right above our
+        heads and flying back to wherever the{" "}
+        <a href="https://www.inaturalist.org/observations?place_id=any&user_id=arshadm&verifiable=any">
+          Land of the Birds
+        </a>{" "}
+        was.
+      </p>
+      <p className="aboutme">
+        That first experience was enough to get me hooked on birding. Ever
+        since, I have enjoyed finding and learning more about all the different
+        types of birds that I paid little mind to before. The world is a vast
+        and extraordinary place when we pay just a little more attention to it.{" "}
+      </p>
+      <h2>Movies</h2>
+      <p className="aboutme">
+        I love watching movies! My family and I are big movie people, we went
+        from watching one movie every friday night to almost every night. My
+        parents' most prized posession is their makeshift theater room in what
+        should technically be our dining room, complete with a bose surround
+        sound system and 120" projecter screen setup. I'm not particulary great
+        at writing (as you can probably tell) but I really want to get better so
+        I try to practice by writing reviews of the movies I watch on{" "}
+        <a href="https://letterboxd.com/arshaddmohammad/">letterboxd</a>! I also
+        just love being able to keep track of every single movie I've seen,
+        when, and who I saw it with :&#41;
+      </p>
+      <h2>Guitar</h2>
+      <p className="aboutme">
+        I play the guitar. No exclamation point, nothing worth being excited
+        about. I am so bad. But nothing beats the feeling of getting to hear a
+        song you love from your own hands, even if it might be wildly out of
+        tempo. Here's a{" "}
+        <a href="https://open.spotify.com/playlist/1utR72Ati6LGTs1vybR5iG?si=dea281e492e3403b">
+          playlist
+        </a>{" "}
+        of every song I have learned and forgotten.
+      </p>
+    </div>
+  );
+}
